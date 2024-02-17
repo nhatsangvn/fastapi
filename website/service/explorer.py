c96f2607ac66b4dc1063717b1676423b8eaadc74
Sang Nguyen <sang@neux.io>
2024-02-17 19:10:33 +0000

    feat: add data / web /service explorer

--- website/service/explorer.py
+++ website/service/explorer.py
@@ -1,11 +1,11 @@
 from model.explorer import Explorer
-import fake.explorer as data
+import data.explorer as data
 
 def get_all() -> list[Explorer]:
     return data.get_all()
 
 def get_one(name: str) -> Explorer | None:
-    return data.get(name)
+    return data.get_one(name)
 
 def create(explorer: Explorer) -> Explorer:
     return data.create(explorer)
@@ -16,5 +16,5 @@ def replace(id, explorer: Explorer) -> Explorer:
 def modify(id, explorer: Explorer) -> Explorer:
     return data.modify(id, explorer)
 
-def delete(id, explorer: Explorer) -> bool:
+def delete(id) -> bool:
     return data.delete(id)
